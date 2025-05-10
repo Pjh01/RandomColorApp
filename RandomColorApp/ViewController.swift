b477ebc6107fbc0651c0884ed229d6f33739e79a
Pjh01 <estelle0102@naver.com>
2025-05-10 17:12:30 +0900

    Feat: changeColor / reset 버튼 이벤트 구현

--- RandomColorApp/ViewController.swift
+++ RandomColorApp/ViewController.swift
@@ -18,5 +18,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func changeColorButtonTapped(_ sender: Any) {
+        let randomRed = CGFloat.random(in: 0...1)
+        let randomGreen = CGFloat.random(in: 0...1)
+        let randomBlue = CGFloat.random(in: 0...1)
+        let randomColor = UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        
+        view.backgroundColor = randomColor
+        colorLabel.text = "R: \(Int(randomRed * 255)), G: \(Int(randomGreen * 255)), B: \(Int(randomBlue * 255))"
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        view.backgroundColor = .white
+        colorLabel.text = "R: 255, G: 255, B: 255"
+    }
 }
 
